eeacc371132cfcf4414b2ce5f6265ed8b43a8f36
Zack2626-ok <thezach2626@gmail.com>
2026-03-10 15:44:15 +0700

    bổ sung thiêu bài table

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Layout } from "antd";
+import { Button, Form, Input, Layout, Table } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
 import Modal from "antd/es/modal/Modal";
@@ -9,6 +9,27 @@ const Dashboard = () => {
   const onSubmit = (value: string) => {
     console.log(value);
   };
+
+  const colum = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+    },
+  ];
+
+  const data = [
+    { key: 1, name: "toan", email: "toan123@gmail.com", role: "user" },
+    { key: 2, name: "duy", email: "duy1123@gmail.com", role: "admin" },
+    { key: 3, name: "truong", email: "truong123123@gmail.com", role: "user" },
+  ];
 
   const onSubmitUser = (value: string) => {
     console.log(value);
@@ -42,6 +63,12 @@ const Dashboard = () => {
           </Button>
         </Form.Item>
       </Form>
+
+      {/* bai 3 */}
+      <Table columns={colum} dataSource={data} />
+
+      {/* bai 4 */}
+
       <Button onClick={() => setOpen(true)}>Mo nhap User</Button>
 
       <Modal
@@ -59,7 +86,7 @@ const Dashboard = () => {
           >
             <Input type={"email"} placeholder="nhap email" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item name={"role"}>
             <Input type={"radio"} name="role" value={"user"} />
             <span>User</span>
             <Input type={"radio"} name="role" value={"admin"} />
